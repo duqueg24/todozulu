fbb371cec8f0f2981a9b2b6bf1376ee7d33131cc
duqueg24 <78827687+duqueg24@users.noreply.github.com>
2021-08-28 07:51:04 -0500

    Update todo

--- todozulu.common/Responses/Response.cs
+++ todozulu.common/Responses/Response.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace todozulu.common.Responses
 {
-    class Response
+    public class Response
     {
         public bool IsSuccess { get; set; }
         public string Message { get; set; }
